3610bdc098c779e0b9237ac8db963b28a5bc654a
Aldeide <anthony.surleraux@gmail.com>
2022-06-25 19:30:05 +0000

    Deploying to gh-pages from  @ 7e5a6aaa5c0bc17a25c18ac95634574a89c63778 🚀

--- js/gemselect.js
+++ js/gemselect.js
@@ -1,64 +1,23 @@
 ﻿function renderjQueryComponents() {
-    const inputField = document.querySelector('.chosen-value');
-    const dropdown = document.querySelector('.value-list');
-    const dropdownArray = [...document.querySelectorAll('li')];
-    console.log(typeof dropdownArray)
-    
-    let valueArray = [];
-    dropdownArray.forEach(item => {
-        valueArray.push(item.textContent);
+    $(document).ready(function () {
+        Reload();
     });
+}
 
-    const closeDropdown = () => {
-        dropdown.classList.remove('open');
-    }
-
-    inputField.addEventListener('input', () => {
-        dropdown.classList.add('open');
-        let inputValue = inputField.value.toLowerCase();
-        let valueSubstring;
-        if (inputValue.length > 0) {
-            for (let j = 0; j < valueArray.length; j++) {
-                if (!(inputValue.substring(0, inputValue.length) === valueArray[j].substring(0, inputValue.length).toLowerCase())) {
-                    dropdownArray[j].classList.add('closed');
-                } else {
-                    dropdownArray[j].classList.remove('closed');
-                }
-            }
-        } else {
-            for (let i = 0; i < dropdownArray.length; i++) {
-                dropdownArray[i].classList.remove('closed');
-            }
+function Reload() {
+    $('.select2').select2({
+        placeholder: "Select",
+    }).on('select2:unselecting', function () {
+        $(this).data('unselecting', true);
+    }).on('select2:opening', function (e) {
+        if ($(this).data('unselecting')) {
+            $(this).removeData('unselecting');
+            e.preventDefault();
         }
-    });
-
-    dropdownArray.forEach(item => {
-        item.addEventListener('click', (evt) => {
-            inputField.value = item.textContent;
-            dropdownArray.forEach(dropdown => {
-                dropdown.classList.add('closed');
-            });
-        });
-    })
-
-    inputField.addEventListener('focus', () => {
-        inputField.placeholder = 'Type to filter';
-        dropdown.classList.add('open');
-        dropdownArray.forEach(dropdown => {
-            dropdown.classList.remove('closed');
-        });
-    });
-
-    inputField.addEventListener('blur', () => {
-        inputField.placeholder = 'Select state';
-        dropdown.classList.remove('open');
-    });
-
-    document.addEventListener('click', (evt) => {
-        const isDropdown = dropdown.contains(evt.target);
-        const isInput = inputField.contains(evt.target);
-        if (!isDropdown && !isInput) {
-            dropdown.classList.remove('open');
-        }
-    });
+    }).on('change.select2', function (e) {
+        var selectedVal = e.target.value;
+        DotNet.invokeMethodAsync('Warrior', 'UpdateGem', selectedVal);
+        //BlazorApp - Your Application DLL Name
+        //UpdateModel - Function Name [JSInvokable]
+    });;
 }
